b9be97e60723ef5bc7f8a70d78cf2c5a73d59149
x-winner <x-winner@xchainlab.io>
2018-10-24 14:35:15 +0800

    Initial commit

--- logger.go
+++ logger.go
@@ -1,4 +1,4 @@
-package main
+package zlog
 
 import (
 	"encoding/json"
@@ -27,25 +27,25 @@ type logFilePath struct {
 	Hook  *lumberjack.Logger
 }
 
-var logger *zap.Logger
-
-func init() {
+func New() *zap.Logger {
 	logcfg := logConfig{
 		Desc:      "development",
 		Level:     "debug",
 		Stdout:    true,
 		Encoding:  "console",
 		AddCaller: true,
-		Color:     false,
-		FilesOut:  true,
-		/*
+		Color:     true,
+		FilesOut:  false,
+		LogsPath: []*logFilePath{{
+			Level: "debug",
 			Hook: &lumberjack.Logger{
 				Filename:   "./logs/gaven.log", // Filename is the file to write logs to.
 				MaxSize:    1024,               // megabytes
 				MaxAge:     7,                  // days
 				MaxBackups: 3,                  // the maximum number of old log files to retain.
 			},
-		*/
+		},
+		},
 	}
 	exists, err := pathExists("./logs.json")
 	if err != nil {
@@ -60,59 +60,80 @@ func init() {
 			panic(err)
 		}
 	}
-
-	// Print only logs above the loggerLevel.
-	loggerLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		switch logcfg.Level {
-		case "debug":
-			return lvl >= zap.DebugLevel
-		case "info":
-			return lvl >= zap.InfoLevel
-		case "error":
-			return lvl >= zap.ErrorLevel
-		default:
-			return lvl >= zap.InfoLevel
-		}
-	})
-	fileWriter := zapcore.AddSync(logcfg.LogsPath[1].Hook)
 	// Output should also go to standard out.
 	consoleDebugging := zapcore.Lock(os.Stdout)
 	var encoderConfig zapcore.EncoderConfig
 	var fileEncoder, consoleEncoder zapcore.Encoder
 	if strings.EqualFold(logcfg.Desc, "production") {
 		encoderConfig = zap.NewProductionEncoderConfig()
-	}
-	if strings.EqualFold(logcfg.Desc, "development") {
+	} else if strings.EqualFold(logcfg.Desc, "development") {
 		encoderConfig = zap.NewDevelopmentEncoderConfig()
+	} else {
+		fmt.Println("'" + logcfg.Desc + "' in the configuration file for desc is an invalid value, it could be 'development' or 'production'")
+		os.Exit(1)
 	}
 	if strings.EqualFold(logcfg.Encoding, "json") {
 		fileEncoder = zapcore.NewJSONEncoder(encoderConfig)
 		consoleEncoder = zapcore.NewJSONEncoder(encoderConfig)
-	}
-	if strings.EqualFold(logcfg.Encoding, "console") {
+	} else if strings.EqualFold(logcfg.Encoding, "console") {
 		fileEncoder = zapcore.NewConsoleEncoder(encoderConfig)
 		if logcfg.Color {
 			encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 		}
 		consoleEncoder = zapcore.NewConsoleEncoder(encoderConfig)
+	} else {
+		fmt.Println("'" + logcfg.Encoding + "' in the configuration file for encoding is an invalid value, it could be 'json' or 'console'")
+		os.Exit(1)
 	}
 	var cores []zapcore.Core
 	if !logcfg.Stdout && !logcfg.FilesOut || logcfg.Stdout {
-		cores := append(cores, zapcore.NewCore(consoleEncoder, consoleDebugging, loggerLevel))
+		cores = append(cores, zapcore.NewCore(consoleEncoder, consoleDebugging, getLevel(logcfg.Level)))
+	}
+	if logcfg.FilesOut {
+		if len(logcfg.LogsPath) > 0 {
+			for i := 0; i < len(logcfg.LogsPath); i++ {
+				cores = append(cores, zapcore.NewCore(fileEncoder, zapcore.AddSync(logcfg.LogsPath[i].Hook), getLevel(logcfg.LogsPath[i].Level)))
+			}
+		}
 	}
 	core := zapcore.NewTee(cores...)
-	/*
-		core := zapcore.NewTee(
-			// Printed in the console.
-			zapcore.NewCore(consoleEncoder, consoleDebugging, loggerLevel),
-			// Printed in file.
-			zapcore.NewCore(fileEncoder, fileWriter, loggerLevel),
-		)
-	*/
 	// From a zapcore.Core to construct a Logger.
+	var logger *zap.Logger
 	if logcfg.AddCaller {
 		logger = zap.New(core, zap.AddCaller())
 	} else {
 		logger = zap.New(core)
 	}
+	logger.Info("Default Logger init success")
+	return logger
+}
+
+func getLevel(level string) zapcore.Level {
+	switch strings.ToLower(level) {
+	case "panic", "dpanic":
+		return zapcore.PanicLevel
+	case "fatal":
+		return zapcore.FatalLevel
+	case "error":
+		return zapcore.ErrorLevel
+	case "warn", "warning":
+		return zapcore.WarnLevel
+	case "info":
+		return zapcore.InfoLevel
+	case "debug":
+		return zapcore.DebugLevel
+	default:
+		return zapcore.DebugLevel
+	}
+}
+
+func pathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
--- logger_test.go
+++ logger_test.go
@@ -1,4 +1,4 @@
-package main
+package zlog
 
 import (
 	"go.uber.org/zap"
@@ -6,9 +6,12 @@ import (
 )
 
 func TestLogger(t *testing.T) {
+	logger := New()
 	logger.Debug("This is a DEBUG message")
 	logger.Info("This is an INFO message")
 	logger.Info("This is an INFO message with fields", zap.String("region", "us-west"), zap.Int("id", 2))
 	logger.Warn("This is a WARN message")
 	logger.Error("This is an ERROR message")
+	logger.Fatal("This is an Fatal")
+	logger.Panic("This is an Panic")
 }
